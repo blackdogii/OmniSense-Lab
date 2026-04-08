cc0ddd948262447f8aa92cbef0fe0e36316ce085
蔡明勳 <37948725+blackdogii@users.noreply.github.com>
2026-04-08 10:27:30 +0800

    更新readme

--- firmware/OmniSense_Main/Config.h
+++ firmware/OmniSense_Main/Config.h
@@ -13,7 +13,7 @@
 #define CHARACTERISTIC_UUID_TX (uint16_t)0xFFE1
 #define CHARACTERISTIC_UUID_RX (uint16_t)0xFFE2
 
-#define BLE_DEVICE_NAME "Omni01"
+#define BLE_DEVICE_NAME "OmniSense_60"
 
 #define OMNISENSE_VER_X   0
 #define OMNISENSE_VER_FW  2
